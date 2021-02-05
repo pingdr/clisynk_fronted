83c2c76774974be0a337a9904210db0a2c7eb38b
mukutprasad <mukutprasad.pingdr@gmail.com>
2021-02-05 19:14:30 +0530

    All Block & Field is Tentative Implemented

--- src/app/internal/documents/documents.module.ts
+++ src/app/internal/documents/documents.module.ts
@@ -9,6 +9,13 @@ import {GridsterModule} from 'angular-gridster2';
 import { CKEditorModule } from 'ng2-ckeditor';
 import { TreeViewModule } from '@progress/kendo-angular-treeview';
 import { MenusModule } from '@progress/kendo-angular-menu';
+import { UploadsModule } from '@progress/kendo-angular-upload';
+import { EditorModule } from '@progress/kendo-angular-editor';
+import { LabelModule } from '@progress/kendo-angular-label';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
 
 const routes: Routes = [
     {
@@ -36,6 +43,13 @@ const routes: Routes = [
         SharedModule,
         TreeViewModule,
         MenusModule,
+        UploadsModule,
+        EditorModule,
+        LabelModule,
+        InputsModule,
+        LayoutModule,
+        DateInputsModule,
+        DropDownsModule,
         RouterModule.forChild(routes)
     ],
     declarations: [
