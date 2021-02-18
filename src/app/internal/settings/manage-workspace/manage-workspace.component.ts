eac521128275648b729e285c98e6c9720c5ddb7b
Gondaliya Parth <gondaliyaparth66@gmail.com>
2021-02-18 14:49:23 +0530

    - workspace changes

--- src/app/internal/settings/manage-workspace/manage-workspace.component.ts
+++ src/app/internal/settings/manage-workspace/manage-workspace.component.ts
@@ -17,36 +17,46 @@ export class ManageWorkspaceComponent implements OnInit {
   loader = false;
   selectedWorkspace: any = {};
 
-  constructor(public http: HttpService, private router: Router) { }
+  constructor(public http: HttpService, private router: Router) {
+    this.http.work.subscribe((res) => {
+      if (res) {
+        this.getAllWorkspaces(false);
+      }
+    })
+  }
 
   ngOnInit() {
     this.getAllWorkspaces(false);
     this.loginData = this.http.loginData;
   }
 
-  getAllWorkspaces(isDeleted?){
+  getAllWorkspaces(isDeleted?) {
     this.loader = true;
     const obj: any = {};
     this.http.getData(ApiUrl.WORKSPACE, obj).subscribe(res => {
-        this.allData = res.data;    
-        res.data.map(wps => {
-          wps.backgroundColor = this.http.getRandomColor();
-        });
-        if(isDeleted){
-          let getLoggedUserFromLocalStorage = JSON.parse(localStorage.getItem("loginData"));
-          getLoggedUserFromLocalStorage.activeWorkspaceId = this.allData[0]._id ? this.allData[0]._id : "";
-          localStorage.setItem("loginData", JSON.stringify(getLoggedUserFromLocalStorage));
-          this.selectedWorkspace = getLoggedUserFromLocalStorage.activeWorkspaceId ? res.data.find((wps) => wps._id === getLoggedUserFromLocalStorage.activeWorkspaceId) : {};
-          this.http.updateWorkspaceList(res.data);
-          this.http.workspaceList.subscribe(wps=> this.allData = wps);
-          this.http.updateWorkspace(this.selectedWorkspace);
-        } else {
-          this.selectedWorkspace = this.loginData.activeWorkspaceId ? res.data.find((wps) => wps._id === this.loginData.activeWorkspaceId) : {};
-          this.http.updateWorkspaceList(res.data);
-          this.http.workspaceList.subscribe(wps=> this.allData = wps);
-        }
-        this.loader = false;
-    }, () => {});
+      this.allData = res.data;
+      res.data.map(wps => {
+        wps.backgroundColor = this.http.getRandomColor();
+      });
+      if (isDeleted) {
+        let getLoggedUserFromLocalStorage = JSON.parse(localStorage.getItem("loginData"));
+        getLoggedUserFromLocalStorage.activeWorkspaceId = this.allData[0]._id ? this.allData[0]._id : "";
+        this.selectedWorkspace = getLoggedUserFromLocalStorage.activeWorkspaceId ? res.data.find((wps) => wps._id === getLoggedUserFromLocalStorage.activeWorkspaceId) : {};
+        localStorage.setItem("loginData", JSON.stringify(getLoggedUserFromLocalStorage));
+        this.http.updateWorkspaceList(res.data);
+        this.http.deleteWorkspaceEvent(true);
+        this.http.workspaceList.subscribe(wps => this.allData = wps);
+        this.http.updateWorkspace(this.selectedWorkspace);
+      } else {
+        this.loginData = this.http.loginData;
+        this.selectedWorkspace = this.loginData.activeWorkspaceId ? res.data.find((wps) => wps._id === this.loginData.activeWorkspaceId) : {};
+        console.log(this.selectedWorkspace);
+
+        // this.http.updateWorkspaceList(res.data);
+        // this.http.workspaceList.subscribe(wps=> this.allData = wps);
+      }
+      this.loader = false;
+    }, () => { });
   }
 
   openEditWorkspace(data) {
@@ -57,19 +67,21 @@ export class ManageWorkspaceComponent implements OnInit {
     this.workspaceId = workspaceId;
     this.http.showModal(template, 'xs');
   }
-  
+
   changeStatus(status) {
     const obj = {
-        status: status,
-        workspaceId: this.workspaceId
+      status: status,
+      workspaceId: this.workspaceId
     };
-    this.http.deleteWorkspace(ApiUrl.WORKSPACE+`/${this.workspaceId}`).subscribe(() => {
+    this.http.deleteWorkspace(ApiUrl.WORKSPACE + `/${this.workspaceId}`).subscribe(() => {
       if (status === 2) {
-          this.http.openSnackBar('Workspace Completed Successfully');
+        this.http.openSnackBar('Workspace Completed Successfully');
+        this.getAllWorkspaces(true);
       } else {
-          this.http.openSnackBar('Workspace Deleted Successfully');
+        this.http.openSnackBar('Workspace Deleted Successfully');
+        this.getAllWorkspaces(true);
       }
-      this.getAllWorkspaces(true);
+      // this.getAllWorkspaces(true);
       // this.reload('settings/manage-workspace');
     });
   }
