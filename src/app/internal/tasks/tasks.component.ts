56e8ccfbdd064dc8a9eef2695794ebd189f2ae4e
Raj Patel <rajangular.pingdr@gmail.com>
2021-06-01 16:53:09 +0530

    task select deselect and delete functionality done

--- src/app/internal/tasks/tasks.component.ts
+++ src/app/internal/tasks/tasks.component.ts
@@ -6,12 +6,13 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {TableModel} from '../../shared/models/table.common.model';
 import {HttpService} from '../../services/http.service';
-import {Subject, Subscription} from 'rxjs';
+import {forkJoin, Subject, Subscription} from 'rxjs';
 import {ApiUrl} from '../../services/apiUrls';
 import { AddTaskComponent } from 'src/app/shared/modals/add-task/add-task.component';
 import * as _ from 'lodash'
 import { NewEditTaskComponent } from '../../shared/modals/new-edit-task/new-edit-task.component';
 import { DeleteComponent } from 'src/app/shared/modals/delete/delete.component';
+import { MatCheckboxChange } from '@angular/material';
 @Component({
     selector: 'app-tasks', 
     templateUrl: './tasks.component.html',
@@ -63,7 +64,6 @@ export class TasksComponent implements OnInit, OnDestroy {
                 this.myModel.allData = res.data.data;
                 this.myModel.count = res.data.count;
                 this.tasks = res.data.data;
-
                 /* Logic for making flat OLD 
                     let t:[] = res.data.data.map(x => x.data);
                     
@@ -84,6 +84,54 @@ export class TasksComponent implements OnInit, OnDestroy {
                 */
 
             });
+    }
+
+    selectedTasks: Task[] = [];
+    onTaskSelectionChange(task: Task) {
+        let foundIndex = this.selectedTasks.findIndex(x => x._id == task._id);
+        /* If not found then push otherwise pop the task */
+        if (foundIndex == -1) {
+            this.selectedTasks.push(task);
+        } else {
+            this.selectedTasks.splice(foundIndex, 1);
+        }
+        console.log(this.selectedTasks);
+    }
+    
+    selectDeselectAllTasks(allSelect:MatCheckboxChange) {
+        if (allSelect.checked) {
+            this.selectedTasks.splice(0, this.selectedTasks.length);
+            this.selectedTasks.push(...this.tasks);
+        } else {
+            this.selectedTasks.splice(0, this.selectedTasks.length);
+        }
+        console.log(this.selectedTasks);
+    }
+
+    loader = false;
+    deleteSelectedTasks() {
+        const requests: any[] = [];
+
+        this.selectedTasks.forEach(task => {
+            const obj = {
+                status: TaskStatus.DELETE,
+                taskId: task._id
+            };
+            requests.push(this.http.getData(ApiUrl.UPDATE_TASK, obj));
+        })
+
+        this.loader = true;
+        forkJoin(requests)
+        .pipe(
+            finalize(()=>{
+                this.http.hideModal();
+                this.loader = false;
+            })
+        )
+        .subscribe((res: any[]) => {
+            console.log(res)
+            this.taskList();
+        })
     }
 
     changeStatus(status,task) {
